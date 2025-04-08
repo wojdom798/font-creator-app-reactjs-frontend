16336b77a1bb89c913d795a8d8b5f85b140480cb
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2025-04-08 13:46:14 +0200

    refactor: place the Char10x16 component in a separate file

---
+++ src/Char10x16.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState, SyntheticEvent } from "react";
+import { convertByteToHexString, charArrayToString } from "./utils";
+import Pixel from "./Pixel";
+
+
+function Char10x16()
+{
+  const [charArray, setCharArray] = useState<number[]>([]);
+  const [pixelNumbersActive, setPixelNumbersActive] = useState<boolean>(true);
+
+  useEffect(() =>
+  {
+    setCharArray(Array(10*(16/8)).fill(0) as number[]);
+  }, []);
+
+
+  const handlePixelClick = (pxId: number, pixelState: boolean) =>
+  {
+    // console.log(`pxId: ${pxId}; pixelState: ${pixelState}`);
+
+    const arrayPart = Math.floor((pxId-1) / (10*8));
+    const bytePos = (pxId-1) % 10 + 10 * arrayPart;
+    const bitPos = Math.floor((pxId - 1 - (10*8)*(arrayPart)) / 10);
+
+    // console.log(`byte #${bytePos}; bit #${bitPos}`);
+
+    setCharArray(charArray.map((byte: number, index: number) =>
+    {
+      if (bytePos === index)
+      {
+        if (pixelState === false)
+        {
+          return (byte & ~(1 << bitPos));
+        }
+        return (byte | (1 << bitPos));
+      }
+
+      return byte;
+    }));
+  }
+
+  const generateTable = () =>
+  {
+    const rows = [];
+    
+    let currentRow = [];
+    let cellCount = 1;
+
+    for (let i = 0; i < 16; i++)
+    {
+      for (let j = 0; j < 10; j++)
+      {
+        currentRow.push(
+          <Pixel
+            key={cellCount}
+            id={cellCount}
+            shouldDisplayNumber={pixelNumbersActive}
+            // onPixelClick={ (pxId: number, pixelState: boolean) => { console.log(`pixel ${pxId} clicked`) }}
+            onPixelClick={handlePixelClick}
+          />
+        );
+        cellCount++;
+      }
+      rows.push(<tr key={i}>{currentRow}</tr>);
+      currentRow = [];
+    }
+
+    return (
+      <React.Fragment>
+      {rows}
+      </React.Fragment>
+    );
+  }
+
+  const logCharArrayToConsole = (ev: SyntheticEvent) =>
+  {
+    ev.preventDefault();
+
+    // console.log(charArray);
+    console.log(charArrayToString(charArray));
+  };
+
+
+  return (
+    <div className="table-wrapper-tmp">
+    <div className="button-container">
+      <button
+        onClick={ () => setPixelNumbersActive(!pixelNumbersActive) }
+      >toggle pixel numbering</button>
+      <button
+        onClick={logCharArrayToConsole}
+      >log char array to console</button>
+    </div>
+    <table className="character10x16-table">
+      <tbody>
+        {generateTable()}
+      </tbody>
+    </table>
+    </div>
+  );
+}
+
+export default Char10x16;
